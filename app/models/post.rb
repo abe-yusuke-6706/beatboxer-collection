fc27d08a982e9bb53f8426fed0b9fc8fe0a64812
unknown <yusukeabe000@gmail.com>
2025-07-23 17:56:34 +0900

    add: タグと中間テーブルのモデルを作成

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :bookmarks, dependent: :destroy
     has_many :likes, dependent: :destroy
+    has_many :tags_relations, dependent: :destroy
 
     validates :title, presence: true, length: { maximum: 255 }
     validates :body, presence: true, length: { maximum: 65535 }
