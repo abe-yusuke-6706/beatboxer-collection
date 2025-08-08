c232e172b0b84b67a612db26eefc8d0ac5bedc27
unknown <yusukeabe000@gmail.com>
2025-08-08 22:11:04 +0900

    add: タグの検索結果をposts/indexを使用して表示

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,25 @@
 class TagsController < ApplicationController
+  skip_before_action :require_login
+  skip_before_action :check_mfa
+  before_action :set_collect_data
+
   def index
     @tags = Tag.all.page(params[:page]).per(40)
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+    @posts = @tag.posts.order(id: :desc).page(params[:page]).per(12)
+
+    render template: "posts/index"
+  end
+
+  private
+
+  def set_collect_data
+    if current_user
+      @bookmarked_post = current_user.bookmark_posts.pluck(:id)
+      @liked_post = current_user.like_posts.pluck(:id)
+    end
   end
 end
